f25877b1bfa6e6318eba8c22ccf7ff5aca0cee6a
sheng yeong <chia.shengyeong@gmail.com>
2022-11-22 16:12:09 +0800

    code styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'highlight.js/styles/darcula.css';
 import 'styles//index.scss';
 
 function MyApp({ Component, pageProps }) {
