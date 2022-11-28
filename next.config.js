e85d3765325780880aa86e7eaf38a82271684b81
sheng yeong <chia.shengyeong@gmail.com>
2022-11-28 11:17:48 +0800

    changing strict mode to false, so can use findDOMNode

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     SANITY_DATASET_NAME: process.env.SANITY_DATASET_NAME,
     SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID
   },
-  reactStrictMode: true,
+  reactStrictMode: false,
 }
 
 module.exports = nextConfig
